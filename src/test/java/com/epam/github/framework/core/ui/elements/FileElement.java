769c621866ed2f08dac041232f4644b4287c0650
Aliaksei Breilian <aliaksei_breilian@epam.com>
2018-07-16 16:48:55 +0300

    Update FileElement.java

--- src/test/java/com/epam/github/framework/core/ui/elements/FileElement.java
+++ src/test/java/com/epam/github/framework/core/ui/elements/FileElement.java
@@ -4,17 +4,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class FileElement extends Element {
+    
+    @FindBy(xpath = ".//div[@id='last-commit']")
+    private WebElement commit;
+    @FindBy(xpath = ".//div[@id='last-commit']")
+    private WebElement itemElement;
 
     public FileElement(WebElement webElement) {
         super(webElement);
     }
 
     public void selectItem(String itemName) {
-        webElement.findElement(By.xpath("//tr//span/a[contains(text(), '" + itemName + "')]")).click();
+        itemElement.click();
+        //webElement.findElement(By.xpath("//tr//span/a[contains(text(), '" + itemName + "')]")).click();
     }
 
     public String getTimeOfCommit() {
         //selectItem(itemName);
-        return webElement.findElement(By.xpath("//div//relative-time")).getText();
+        return commit.getText();
     }
 }
